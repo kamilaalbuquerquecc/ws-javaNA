a47840f5dd22e1a35d177c2dd06a6b7e0da596bf
Kamila <kamila.albuquerque@ccc.ufcg.edu.br>
2021-02-20 11:00:47 -0300

    Criei mais pastas pra organizar

---
+++ Enumeracoes/Aula115/src/entities/enums/OrderStatus.java
@@ -0,0 +1,12 @@
+package entities.enums;
+/*
+ * Observacoes da classe enum
+ * tranforma o enum para string, o mesmo nome declarado
+ */
+public enum OrderStatus {
+	PENDING_PAYMENT, //AGUARDANDO PAGAMENTO
+	PROCESSING,//PROCESSANDO
+	SHIPPED, //ENVIADO
+	DELIVERED; //ENTREGUE
+
+}
